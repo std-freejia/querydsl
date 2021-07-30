28c251a31fb73c692fdad6ad5842b6cd2323abca
std-freejia <std-freejia@gmail.com>
2021-07-30 14:26:53 +0900

    QueryDSL 설정과 검증
    [build.gradle에 설정 추가]
    plugin, dependency, compileQuerydsl
    
    [엔티티 명에 Q를 붙인 클래스 생성됨]
    1) 빌드 하거나 Gradle>querydsl>Tasks>other/compileQuerydsl 실행.
    2) 프로젝트>build>generated>querysql>클래스
    3) generated  하위의 Q붙은 클래스들은 깃헙에 올리지 말기. build 하위는 전부 gitignore

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,40 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		//QHello qHello = new QHello("h"); // allias를 넣어준다.
+		QHello qHello = QHello.hello; // 생성 방법.
+
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
 	}
 
 }
