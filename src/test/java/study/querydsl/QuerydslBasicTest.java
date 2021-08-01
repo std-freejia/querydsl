6202b318cc061c94dd5d9cfe2d781370d1309faf
std-freejia <std-freejia@gmail.com>
2021-08-01 12:28:02 +0900

    기본문법 시작 - JPQL vs Querydsl
    Querydsl은 파라미터 바인딩 자동으로 처리해준다. 그리고 컴파일타임에 에러를 잡을 수 있다.
    
    그리고 미리 컴파일한 Q타입 클래스를 만들어 두기 때문에 개발 중에 컴파일 오류를 피할 수 있다.
    
    동시성 문제는 JPAQueryFactory를 생성할 때 제공하는 EntityManager(em)에 달려있다.
    스프링 프레임워크는 여러 쓰레드에서 동시에 같은 EntityManager에 접근해도, 트랜잭션 마다 별도의 영속성 컨텍스트를 제공하기 때문에, 동시성 문제는 걱정하지 않아도 된다.

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,7 +1,71 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+import study.querydsl.entity.Team;
+
+import javax.persistence.EntityManager;
 
 @SpringBootTest
+@Transactional
 public class QuerydslBasicTest {
+
+    @Autowired EntityManager em;
+    JPAQueryFactory queryFactory;
+
+    // JPQL 작성 후 QueryDSL 로 바꾸면서 비교.
+    @Test
+    public void startJPQL(){ // 에러가 런타임에 발생한다.
+        // member1 을 찾아라.
+        Member qlString =
+                em.createQuery("select m from Member m where m.username = :username", Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+        Assertions.assertThat(qlString.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void startQuerydsl(){ // 컴파일타임에 오류가 잡힌다.
+
+        QMember m = new QMember("mem"); // "m" 변수 명에 별칭을 주는건데 별로 안중요.
+
+        Member findMember = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1"))
+                .fetchOne();
+        // 파라미터 바인딩을 Prepared statement 에 바인딩.
+        // Prepared statement : 자주 쓰는 sql 을 미리 DB가 이해하기 쉽게 해석해 놓은 것.
+        // 대량 쿼리 처리 및 SQLinjection 에 대한 약간의 방어 효과 있음
+        Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @BeforeEach // @Test 실행 전 마다 데이터 미리 세팅하기
+    public void before(){
+
+        queryFactory = new JPAQueryFactory(em); // 필드레벨에서 처리해도 동시성 문제 없다.
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
 }
