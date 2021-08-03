96769426b535bc46acf2515aa00bfd44dae03b8a
std-freejia <std-freejia@gmail.com>
2021-08-03 17:02:32 +0900

    서브 쿼리
    쿼리 안의 쿼리.
    from 을 제외하고 where 또는 select 절 등에 쿼리를 넣는다.
    JPA JPQL은 from 절의 서브쿼리(인라인 뷰)는 지원하지 않는다.
    
    해결방안 : 서브쿼리를 join으로 변경한다. (대부분 가능하다.) 또는 쿼리를 2번 분리해서 실행한다.

--- src/test/java/study/querydsl/QuerydslBasicJoinTest.java
+++ src/test/java/study/querydsl/QuerydslBasicJoinTest.java
@@ -1,6 +1,7 @@
 package study.querydsl;
 
 import com.querydsl.core.Tuple;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
@@ -16,6 +18,7 @@ import javax.persistence.PersistenceUnit;
 
 import java.util.List;
 
+import static com.querydsl.jpa.JPAExpressions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static study.querydsl.entity.QMember.member;
 import static study.querydsl.entity.QTeam.team;
@@ -187,6 +190,87 @@ public class QuerydslBasicJoinTest { /** [기본 문법 조인 ] */
         // isLoaded() 캐시에 로딩된 엔티티인지 여부를 알려준다.
         boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam()); // "Team 이 로딩됬니?"
         assertThat(loaded).as("패치 조인 적용").isTrue(); // Team 까지 로딩 됨.
+    }
+
+    /** 서브 쿼리
+     *
+     * 예) 나이가 가장 많은 회원 조회
+     * */
+    @Test
+    public void subQuery() throws Exception{
+
+        // member 별칭을 따로 줘야 할 때 별칭 다른 객체를 생성
+        QMember memberSub = new QMember("memberSub");
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(
+                        select(memberSub.age.max())
+                                .from(memberSub)
+                ))
+                .fetch();
+        /* 나이가 가장 많은 사람의 나이를 먼저 구한다.
+        *  서브쿼리:  JPAExpressions.select(memberSub.age.max()).from(memberSub)  */
+
+        assertThat(result).extracting("age").containsExactly(40);
+    }
+
+    @Test /** 서브쿼리 goe(greater or equal) 사용:  나이가 평균 이상인 회원 조회 */
+    public void subQueryGoe() throws Exception{
+
+        QMember memberSub = new QMember("memberSub");
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.goe(
+                        select(memberSub.age.avg())
+                                .from(memberSub)
+                ))
+                .fetch();
+
+        assertThat(result).extracting("age").containsExactly(30, 40);
+    }
+
+    @Test /** 서브쿼리 in : 10살 초과인 회원 */
+    public void subQueryIn() throws Exception{
+
+        QMember memberSub = new QMember("memberSub");
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.in(
+                        select(memberSub.age)
+                                .from(memberSub)
+                                .where(memberSub.age.gt(10)) // 10살 초과.
+                ))
+                .fetch();
+
+        assertThat(result).extracting("age").containsExactly(20, 30, 40);
+    }
+
+
+    @Test /** select 절 서브쿼리 : 이름, 평균나이 조회   */
+    public void selectSubQuery() throws Exception{
+
+        QMember memberSub = new QMember("memberSub");
+
+        List<Tuple> result = queryFactory
+                .select(member.username,
+                        select(memberSub.age.avg())
+                            .from(memberSub))
+                .from(member)
+                .fetch();
+
+        /** [ from 절의 서브쿼리 한계 ]
+         * from 절의 서브쿼리(인라인 뷰)는 지원하지 않는다.
+         * where, select 등 다른 절에서 서브쿼리 가능하다.
+         * 해결방안 : 서브쿼리를 join으로 변경한다. (대부분 가능하다.) 또는 쿼리를 2번 분리해서 실행한다.
+         * */
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple.get(member.username) = " + tuple.get(member.username));
+            System.out.println("tuple = " + tuple.get(select(memberSub.age.avg()).from(memberSub)));
+        }
     }
 
     @BeforeEach // @Test 실행 전 마다 데이터 미리 세팅하기
