b2b115d867bdff1b0541763b7759c5c46f93e3a8
std-freejia <std-freejia@gmail.com>
2021-08-23 23:47:06 +0900

    스프링 데이터 JPA 리포지토리로 변경
    스프링 데이터 JPA가 save(), findById(), findByUsername() 등을 쿼리메소드로도 기본적으로 지원한다.

---
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -0,0 +1,38 @@
+package study.querydsl.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+    @Autowired
+    EntityManager em;
+    @Autowired MemberRepository memberRepository;
+
+
+    @Test
+    public void basicTest(){
+        Member member = new Member("member1", 10);
+        memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(member.getId()).get();
+        // get() 쓰면안되는데, 여기는 연습이니까.
+        assertThat(findMember).isEqualTo(member);
+
+        List<Member> result = memberRepository.findAll();
+        assertThat(result).containsExactly(member);
+
+        List<Member> result2 = memberRepository.findByUsername("member1");
+        assertThat(result2).containsExactly(member);
+    }
+}
