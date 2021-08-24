709e8da66d663b84152016a3d2e6c746dc557399
std-freejia <std-freejia@gmail.com>
2021-08-24 19:52:13 +0900

    인터페이스 지원 - QuerydslPredicateExecutor
    한계점
    1) 조인 불가.
    2) 서비스나 컨트롤러 로직(클라이언트 코드)이 Querydsl이라는 구현기술에 의존해야 한다.

--- src/main/java/study/querydsl/repository/MemberRepository.java
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -1,12 +1,13 @@
 package study.querydsl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import study.querydsl.entity.Member;
 
 import java.util.List;
 
 /** MemberRepositoryCustom 도 상속받는다.  */
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom, QuerydslPredicateExecutor<Member> {
 
     // 쿼리메소드  select m from Member m where m.username = ?
     List<Member> findByUsername(String username);
@@ -14,4 +15,9 @@ public interface MemberRepository extends JpaRepository<Member, Long>, MemberRep
     /**  특정한 기능 or 화면 or API에 맞춰진 조회 쿼리가 필요할 때,
      * 인터페이스 없이 구현체를 바로 만드는 방법도 좋은 방법이다.
      * 기본으로 Custom을 쓰는것이 좋지만, 설계에 따라 유연하게 레포지토리를 만들자. */
+
+    /**   QuerydslPredicateExecutor 한계점. 실무에서 권장하지 않는 이유.
+     1) 조인 불가.
+     2) 서비스나 컨트롤러 로직(클라이언트 코드)이 Querydsl이라는 구현기술에 의존해야 한다.
+     */
 }
--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
@@ -16,6 +17,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static study.querydsl.entity.QMember.member;
 
 @SpringBootTest
 @Transactional
@@ -101,5 +103,34 @@ class MemberRepositoryTest {
 
         assertThat(result.getSize()).isEqualTo(3); // 데이터 사이즈 3인지 확인
         assertThat(result.getContent()).extracting("username").containsExactly("member1", "member2", "member3");
+    }
+
+    /** 인터페이스 QuerydslPredicateExecutor */
+    @Test
+    public void querydslPredicateExecutorTest(){
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        // 데이터 JPA가 제공하는 메소드들에 조건을 줄 수 있다.
+        Iterable<Member> result = memberRepository
+                .findAll(member.age.between(10, 40)
+                        .and(member.username.eq("member1")));
+
+        for (Member findMember : result) {
+            System.out.println("findMember = " + findMember);
+        }
     }
 }
